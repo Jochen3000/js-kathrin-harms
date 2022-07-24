93490b8a9441b45cb5e64e37a6a8cb00fd4166ef
jochen ade <jochen.ade@springer.com>
2022-07-24 12:52:14 +0200

    copy code from webflow

--- local.js
+++ local.js
@@ -1,21 +1,34 @@
+// Only trigger on mobile
+const mediaQuery = window.matchMedia('(max-width: 479px)')
+
+// Check if the media query is true
+if (mediaQuery.matches) {
+
+  //Move divs to destination 
+  const newDestination = document.querySelector('.new-home'); 
+
+  // foreach in action
+  const myListFunction = document.querySelectorAll('.slider .slidergrid');
+  myListFunction.forEach((element) => {
+    newDestination.appendChild(element);
+  });
+}
+
 // show backtotop button when footer is intersecting
-const backToTop = document.querySelector('.backtoplink');
-backToTop.style.display = 'none';
+const backToTop = document.querySelector('.backuplink');
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       const intersecting = entry.isIntersecting
       if(intersecting){
         backToTop.style.opacity = '1';
-        backToTop.style.display = 'block';
       } else {
         backToTop.style.opacity = '0';
-        backToTop.style.display = 'none';
       }
     })
 }, { threshold: 0 });
   observer.observe(document.querySelector('.footer-wrap'));
-
+  
 // Hover effect foldericon
 const alwaysTakeMeBlock = document.querySelectorAll('.alwaystakemeblock');
 alwaysTakeMeBlock.forEach(block => {
